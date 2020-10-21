8a98db1f79fa3054d77833a5c2c57d89234fc5aa
KarenCortesT <cortestelloa@gmail.com>
2020-10-21 14:17:53 -0500

    Updates

---
+++ precache-manifest.c3752e47876029bf543aea6cc430ba3a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a020225a2ecb5df15b24ec06872ada3",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "35b158890cae1bdb1de8",
+    "url": "/dashboard/static/css/main.3ec0a6c4.chunk.css"
+  },
+  {
+    "revision": "76db3488035c5abcd5b3",
+    "url": "/dashboard/static/js/2.d11e13f2.chunk.js"
+  },
+  {
+    "revision": "d38009895be181f0b5fdf9b5a49db9e7",
+    "url": "/dashboard/static/js/2.d11e13f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35b158890cae1bdb1de8",
+    "url": "/dashboard/static/js/main.162c33a2.chunk.js"
+  },
+  {
+    "revision": "198d6edb3025da41e22c",
+    "url": "/dashboard/static/js/runtime-main.583e4126.js"
+  },
+  {
+    "revision": "ede6f545425f15185ceb5cc7f3a3d6bc",
+    "url": "/dashboard/static/media/bg.ede6f545.png"
+  },
+  {
+    "revision": "d2bd30657c68c5713d5bc13c0ee92d62",
+    "url": "/dashboard/static/media/sin_conexion.d2bd3065.png"
+  },
+  {
+    "revision": "5432b93dc34921b3066301240f0f19d2",
+    "url": "/dashboard/static/media/users.5432b93d.png"
+  }
+]);
--- src/redux/reducers/reducer.js
+++
@@ -1,5 +0,0 @@
-import initialState from './initialState';
-
-const SET_LOGIN_PENDING ='SET_LOGIN_PENDING';
-const SET_LOGIN_SUCCES = 'SET_LOGIN_SUCCES';
-const SET_LOGIN_ERROR = 'SET_LOGIN_ERROR';
